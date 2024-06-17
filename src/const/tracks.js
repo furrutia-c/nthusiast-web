c6c472f568a70b4d239ffcf346546187ab946773
Fernando Urrutia <f.urrutiac@gmail.com>
2024-06-17 13:17:54 +0200

    feat: Add tracks section to display circuit information and times

---
+++ src/const/tracks.js
@@ -0,0 +1,68 @@
+import { record } from "astro/zod";
+
+export const tracks = [
+    {
+        name: "Nurburgring BTG",
+        length: "20.8 km",
+        image: "/public/img/nurburgring.webp",
+        description: "El circuito de Nürburgring es un circuito de carreras situado en Nürburg, Alemania. Es conocido por ser uno de los circuitos más largos y difíciles del mundo.",
+        record: {
+            car: "Hyundai i30 N",
+            driver: "Manuel Lauck",
+            time: "7:51",
+            year: 2023
+        },
+        times: [
+            {
+                car: "Hyundai i30 N",
+                driver: "Manuel Lauck",
+                time: "7:51",
+                year: 2023
+            },
+            {
+                car: "Hyundai i20 N",
+                driver: "Benjamin Leuchter",
+                time: "7:53",
+                year: 2020
+            },
+            {
+                car: "Hyundai i30 N",
+                driver: "Honda",
+                time: "7:50",
+                year: 2017
+            }
+        ]
+    },
+    {
+        name: "Circuit de Calafat",
+        length: "3.2 km",
+        image: "/public/img/calafat.webp",
+        description: "El circuito de Calafat es un circuito de carreras situado en la localidad de L'Ametlla de Mar, en la provincia de Tarragona, España.",
+        record: {
+            car: "Hyundai i30 N",
+            driver: "Manuel Lauck",
+            time: "1:20",
+            year: 2023
+        },
+        times: [
+            {
+                car: "Hyundai i30 N",
+                driver: "Manuel Lauck",
+                time: "1:20",
+                year: 2023
+            },
+            {
+                car: "Hyundai i20 N",
+                driver: "Benjamin Leuchter",
+                time: "1:22",
+                year: 2020
+            },
+            {
+                car: "Hyundai i30 N",
+                driver: "Honda",
+                time: "1:19",
+                year: 2017
+            }
+        ]
+    }
+]
